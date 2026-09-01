757cf94d4e4bdc4b284acd42c0a7d5ef19743e64
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-09-01 19:13:55 +0700

    Update maritimeSafetyComplianceEngine.js

--- maritimeSafetyComplianceEngine.js
+++ maritimeSafetyComplianceEngine.js
@@ -393,7 +393,50 @@ const MaritimeSafetyComplianceEngineV1 = {
         }
 
 
-        /* =================================================
+        /*
+
+/* =================================================
+   P&I / MARINE INSURANCE DOCUMENTATION
+================================================= */
+
+const pi =
+    condition.pi || {};
+
+const piClubDeclared =
+    pi.clubDeclared === true;
+
+const piCoverCurrent =
+    pi.coverCurrent === true;
+
+const piDocumentationCurrent =
+    pi.documentationCurrent === true;
+
+const piEntryEvidenceAvailable =
+    pi.entryEvidenceAvailable === true;
+
+
+let piStatus =
+    "NOT_ASSESSED";
+
+
+if (
+    piClubDeclared &&
+    piCoverCurrent &&
+    piDocumentationCurrent &&
+    piEntryEvidenceAvailable
+) {
+
+    piStatus =
+        "SIMULATED_DOCUMENTATION_CURRENT";
+
+}
+else {
+
+    piStatus =
+        "SIMULATED_INSURANCE_REVIEW_REQUIRED";
+
+}
+ =================================================
            OVERALL SAFETY ASSESSMENT
         ================================================= */
 
